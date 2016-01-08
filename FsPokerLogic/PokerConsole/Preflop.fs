c353f35af0da6439db3039cf0bc203148e5ec6f1
Mikhail Shilkov <mshilkov@astrata.eu>
2016-01-08 17:04:15 +0100

    Starting preflop

---
+++ FsPokerLogic/PokerConsole/Preflop.fs
@@ -0,0 +1,47 @@
+﻿module Preflop
+
+type Face = 
+  | Ace
+  | King
+  | Queen
+  | Jack
+  | Ten
+  | Nine
+  | Eight
+  | Seven
+  | Six
+  | Five
+  | Four
+  | Three
+  | Two
+
+type Suit = 
+  | Hearts
+  | Diamonds
+  | Clubs
+  | Spades
+
+type Card = 
+  { Face : Face
+    Suit : Suit }
+
+type Hand = 
+  { High: Card 
+    Low: Card}
+
+type Range = Hand seq
+
+let isHandInRange (hand : Hand) (range : Range) : bool = 
+  Seq.contains hand range
+
+let anySuit highFace lowFace : Range = 
+  [{Face = highFace; Suit = Hearts}; 
+  {Face = highFace; Suit = Diamonds}; 
+  {Face = highFace; Suit = Clubs}; 
+  {Face = highFace; Suit = Spades}] |> seq 
+
+let rangeFromMask anySuit (mask: string) : Range =
+  match (mask) with
+    | "AA" -> anySuit Ace Ace
+    | _ -> failwith "Unknown mask"
+  

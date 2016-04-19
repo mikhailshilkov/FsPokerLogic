20773925fb08102759bbc147f55a78d5ec5d3b09
Mikhail Shilkov <shilkov@gmail.com>
2016-04-19 23:32:51 +0200

    Start made hand values

---
+++ Hands/MadeHandValue.fs
@@ -0,0 +1,52 @@
+﻿namespace Cards
+
+open Hands
+
+module MadeHandValue =
+
+  type PairType =
+    | Under
+    | Fifth
+    | Fourth
+    | Third
+    | Second
+    | Top
+    | Over
+
+  type HandValue =
+    | Nothing
+    | Pair of PairType
+
+  let handValue hand board =
+    let boardLength = Seq.length board
+    let boardFaces =
+      board 
+      |> Seq.sortByDescending (fun x -> faceValue x.Face)
+      |> Seq.map (fun x -> x.Face)
+
+    let pairIndex = 
+      boardFaces
+      |> Seq.mapi (fun i x -> (x = hand.Card1.Face || x = hand.Card2.Face, i))
+      |> Seq.filter (fun (x, i) -> x)
+      |> Seq.map snd
+      |> Seq.map ((+) 1)
+      |> Seq.tryHead
+
+    if hand.Card1.Face = hand.Card2.Face then 
+      let biggerBoardCards = boardFaces |> Seq.filter (fun x -> faceValue x > faceValue hand.Card1.Face) |> Seq.length
+      match biggerBoardCards with
+      | i when i = boardLength -> Pair(Under)
+      | 0 -> Pair(Over)
+      | 1 -> Pair(Second)
+      | 2 -> Pair(Third)
+      | 3 -> Pair(Fourth)
+      | 4 -> Pair(Fifth)
+      | _ -> failwith "Board too long?"
+    else 
+      match pairIndex with
+      | Some(1) -> Pair(Top)
+      | Some(2) -> Pair(Second)
+      | Some(3) -> Pair(Third)
+      | Some(4) -> Pair(Fourth)
+      | Some(5) -> Pair(Fifth)
+      | _ -> Nothing
---
+++ Hands/MadeHandValueTests.fs
@@ -0,0 +1,58 @@
+﻿module MadeHandValueTests
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+open Hands
+open Cards.MadeHandValue
+
+let test handS flopS expected =
+  let hand = parseSuitedHand handS
+  let board = parseBoard flopS
+  let actual = handValue hand board
+  Assert.Equal(expected, actual)
+
+
+[<Theory>]
+[<InlineData("7s2c", "AdKdJhTh8c")>]
+let ``Nothing`` handS flopS =
+  Nothing |> test handS flopS
+
+[<Theory>]
+[<InlineData("5h5c", "KdJhTh8c")>]
+let ``Underpair`` handS flopS =
+  Pair(Under) |> test handS flopS
+
+[<Theory>]
+[<InlineData("AhAc", "KdJhTh8c")>]
+let ``Overpair`` handS flopS =
+  Pair(Over) |> test handS flopS
+
+[<Theory>]
+[<InlineData("Kh5c", "Th8cKdJh")>]
+let ``Top pair`` handS flopS =
+  Pair(Top) |> test handS flopS
+
+[<Theory>]
+[<InlineData("Jc5c", "Th8cKdJh")>]
+[<InlineData("QhQc", "KdJhTh8c5d")>]
+let ``Second pair`` handS flopS =
+  Pair(Second) |> test handS flopS
+
+[<Theory>]
+[<InlineData("AcTc", "Th8cKdJh")>]
+[<InlineData("JhJc", "KdQhTh8c5d")>]
+let ``Third pair`` handS flopS =
+  Pair(Third) |> test handS flopS
+
+[<Theory>]
+[<InlineData("Ac8c", "Th8cKdJh")>]
+[<InlineData("9h9c", "KdJhTh8c5d")>]
+let ``Fourth pair`` handS flopS =
+  Pair(Fourth) |> test handS flopS
+
+[<Theory>]
+[<InlineData("Ac3c", "Th8cKdJh3h")>]
+[<InlineData("6h6c", "KdJhTh8c5d")>]
+let ``Fifth pair`` handS flopS =
+  Pair(Fifth) |> test handS flopS
